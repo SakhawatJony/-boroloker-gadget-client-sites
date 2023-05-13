910342cb5b652732aec3a93f53798a4a4eece82e
Sakhawat Hosen <sakhawathosen320@gmail.com>
2023-05-13 22:00:39 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home/Home/Home';
-import Footer from './Pages/Shared/Footer/Footer';
-import Navbar from './Pages/Shared/Navbar/Navbar';
+import router from './Pages/Routes/Routes/Routes';
+
 
 function App() {
   return (
-    <div className="App m-10">
+    <div className='mx-w-[1440] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
      
-      <Navbar></Navbar>
-      <Home></Home>
-      <Footer></Footer>
-
-      <h1 className='text-2xl text-info m-2'>Kaj korte korte sobaik Delete maro miha </h1>
-
-      <button class="btn btn-primary mt-5">Delete Maro</button>
+     
     </div>
   );
 }
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
+
+    const menuItems = <React.Fragment>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/service">Services</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/gallery">Gallery </Link></li>
+       <li><Link to="/login">Contact</Link></li>
+    </React.Fragment>
     return (
-        <div>
-             <h1 className='text-info text-5xl font-bold'>I am Navbar and ami commings soons!!!!!!!</h1>
-            
+        <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-medium">
+              {
+                menuItems
+              }
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">Boroloker Gadget</Link>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 font-medium">
+            {
+                menuItems
+            }
+          </ul>
+        </div>
+      </div>
     );
 };
 
