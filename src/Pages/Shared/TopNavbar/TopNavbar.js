3fc91e7a7dfd32f981e44b21a95a6ee0ed482c1f
saifulemon <463saiful0@gmail.com>
2023-05-14 17:58:57 +0600

    top navbar design successfully added

--- src/Pages/Shared/TopNavbar/TopNavbar.js
+++ src/Pages/Shared/TopNavbar/TopNavbar.js
@@ -1,11 +1,92 @@
-import React from 'react';
-
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from "react";
 
 const TopNavbar = () => {
-    return (
-      <>
-      </>
-    );
+  return (
+    <>
+      {/* top navbar start */}
+      <section className="block w-full h-auto p-2 relative ease-linear bg-[#b8cd06]">
+        <div className="md:container md:mx-auto lg:container lg:mx-auto">
+          {/* top main start */}
+          <div className="flex items-center justify-between gap-3">
+            {/* contact info start */}
+            <div className="block p-0 m-0">
+              <ul className="list-none p-0 m-0">
+                <li className="inline-block mr-5">
+                  <a
+                    href="tel"
+                    className="flex items-center gap-2 font-serif font-normal text-sm cursor-pointer no-underline text-white"
+                  >
+                    <i className="fas fa-phone-alt"></i>
+                    +8801302050981
+                  </a>
+                </li>
+                <li className="inline-block">
+                  <a
+                    href="tel"
+                    className="flex items-center gap-2 font-serif font-normal text-sm cursor-pointer no-underline text-white"
+                  >
+                    <i className="fas fa-envelope"></i>
+                    boroloker-gadget@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* contact info end */}
+
+            {/* discount sec start */}
+            <div className="flex items-center justify-center gap-4">
+              <h3 className=" text-white font-serif font-normal block leading-4 tracking-wide text-sm">
+                30% Discount All New Customers
+              </h3>
+              <button className="text-[#b8cd06] text-xs font-medium bg-white outline-none py-0 px-3 rounded border-transparent uppercase font-sans inline-block leading-6 transition-all hover:bg-[#b8cd06] hover:text-white">
+                Get Register
+              </button>
+            </div>
+            {/* discount sec end */}
+
+            {/* top-right start */}
+            <div className="flex items-center justify-end gap-3">
+              <div className="block">
+                <ul className="list-none flex m-0 p-0 items-center justify-end">
+                  <li className="inline-block mr-3">
+                    <a
+                      className="s relative w-8 h-8 flex items-center justify-center text-sm text-[#b8cd06] bg-white transition-all rounded-full"
+                      href="/wishlist"
+                    >
+                      <i class="fas fa-heart"></i>
+                    </a>
+                  </li>
+                  <li className="inline-block mr-3">
+                    <a
+                      className="s relative w-8 h-8 flex items-center justify-center text-sm text-[#b8cd06] bg-white transition-all rounded-full"
+                      href="/wishlist"
+                    >
+                      <i class="fas fa-cart-plus"></i>
+                    </a>
+                  </li>
+                  <li className="inline-block mr-3">
+                    <a
+                      className="s relative w-8 h-8 flex items-center justify-center text-sm text-[#b8cd06] bg-white transition-all rounded-full"
+                      href="/wishlist"
+                    >
+                      <i class="fas fa-user"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="min-[120px]">
+                <h3 className=" text-black font-serif font-normal leading-5 text-sm"><a href="/login">Sign Up / Login</a></h3>
+              </div>
+            </div>
+            {/* top-right end */}
+          </div>
+          {/* top main end */}
+        </div>
+      </section>
+      {/* top navbar end */}
+    </>
+  );
 };
 
 export default TopNavbar;
